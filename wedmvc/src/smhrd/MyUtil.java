8191527932bce131b4ad9ce4eca72a385d864c2f
smhrd <smhrd@DESKTOP-KB767KO>
2020-09-07 16:59:39 +0900

    -수정완료

--- wedmvc/src/smhrd/MyUtil.java
+++ wedmvc/src/smhrd/MyUtil.java
@@ -4,7 +4,7 @@ public class MyUtil {
    public int hap() {
 	   //1-10까지의 총 합을 구하는 동작
       int sum=0;
-      for(int i=1;i<=10;i++) {
+      for(int i=1;i<=100;i++) {
          sum+=i;
       }
       //길동아 여기좀 수정해봐
