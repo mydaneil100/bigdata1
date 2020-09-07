e33645babfe9df9cdcfae6f15701abad7819ddb1
smhrd <smhrd@DESKTOP-KB767KO>
2020-09-07 16:11:50 +0900

    한글추가

--- wedmvc/src/smhrd/MyUtil.java
+++ wedmvc/src/smhrd/MyUtil.java
@@ -1,5 +1,12 @@
 package smhrd;
 
 public class MyUtil {
-
+   public int hap() {
+	   //1-10까지의 총 합을 구하는 동작
+      int sum=0;
+      for(int i=1;i<=10;i++) {
+         sum+=i;
+      }
+      return sum;
+   }
 }
